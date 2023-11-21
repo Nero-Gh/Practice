edad838abd5d21501221e549db80e9d9cd7560cb
Bismark Kadogbe <67119909+Nero-Gh@users.noreply.github.com>
2023-11-21 20:49:44 +0000

    created a slice to fetch data

--- redux-typescript-react/src/features/users/usersSlice.ts
+++ redux-typescript-react/src/features/users/usersSlice.ts
@@ -1,20 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { User } from "./types";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { User, UserState } from "./types";
 import { RootState } from "../../app/store";
 
-const initialState: Array<User> = [];
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: undefined,
+};
 
-export const usersSlice = createSlice({
-  name: "users",
-  initialState,
-  reducers: {
-    addUser: (state, action) => {
-      state.push(action.payload);
-    },
-  },
+export const fetchUsers = createAsyncThunk("users/fetchUsers", () => {
+  const res = fetch("https://jsonplaceholder.typicode.com/users").then((data) =>
+    data.json()
+  );
+  return res;
 });
 
-export const { addUser } = usersSlice.actions;
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(
+      fetchUsers.fulfilled,
+      (state, action: PayloadAction<Array<User>>) => {
+        state.loading = false;
+        state.users = action.payload;
+      }
+    );
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.loading = false;
+      state.users = [];
+      state.error = action.error.message;
+    });
+  },
+  reducers: {},
+});
 export const userSelector = (state: RootState) => state.userReducer;
-
 export default usersSlice.reducer;
