5bfb370e2ff4dee88d9299607ba985f3d09893c8
Bismark Kadogbe <67119909+Nero-Gh@users.noreply.github.com>
2023-11-22 12:57:20 +0000

    Fix image tag in Card component

--- theme-toggle-js/src/components/Card.jsx
+++ theme-toggle-js/src/components/Card.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Card = () => {
   return (
     <div className="w-full bg-white border bortder-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a
-        href="https://images.pexels.com/photos/16486462/pexels-photo-16486462/free-photo-of-woman-wearing-earmuffs-on-winter-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      <img
+        src="https://images.pexels.com/photos/16486462/pexels-photo-16486462/free-photo-of-woman-wearing-earmuffs-on-winter-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
         className="p-8 rounded-t-lg"
-      ></a>
-      <div className="px-5 pb-5"></div>
+      ></img>
+      <span className="px-5 pb-5 dark:text-gray-100 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </span>
     </div>
   );
 };
