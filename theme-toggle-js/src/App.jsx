2e26a7c62c6273d24b339f556bc5cbb053f41efa
Bismark Kadogbe <67119909+Nero-Gh@users.noreply.github.com>
2023-11-22 12:52:44 +0000

    Refactor theme toggle functionality and update component structure

--- theme-toggle-js/src/App.jsx
+++ theme-toggle-js/src/App.jsx
@@ -1,7 +1,42 @@
+import React, { useEffect } from "react";
+import Card from "./components/Card";
+import ThemeButton from "./components/ThemeButton";
+import { ThemeProvider } from "./contexts/theme";
+
 function App() {
+  const [themeMode, setThemeMode] = React.useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    // const root = window.document.documentElement;
+    // root.classList.remove("light", "dark");
+    // root.classList.add(themeMode);
+
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <>
-      <div>Hello</div>
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeButton />
+            </div>
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
